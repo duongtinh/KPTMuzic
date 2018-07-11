b36cb2e8144bd55a0401a9787f256a6201998160
jack <jack@dk42>
2018-07-11 17:58:25 +0700

    Add folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { StoriesComponent } from './components/stories/stories.component';
+import { AboutComponent } from './components/about/about.component';
+import { VideosComponent } from './components/videos/videos.component';
+import {NglModule} from 'ng-lightning/ng-lightning';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StoriesComponent,
+    AboutComponent,
+    VideosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NglModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
